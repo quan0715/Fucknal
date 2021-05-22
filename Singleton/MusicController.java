d3966f54d7effdbdaf0ffc74510537d308a797b7
AlbertDoggyLin <albertlin2468@gmail.com>
2021-05-22 15:13:14 +0800

    aaa

--- Singleton/MusicController.java
+++ Singleton/MusicController.java
@@ -9,8 +9,8 @@ public class MusicController {
   private MediaPlayer BackGround1MediaPlayer;
   private MediaPlayer BackGround2MediaPlayer;
   private MusicController(){
-    BackGround1MediaPlayer = new MediaPlayer(new Media(getClass().getResource("../Sound/tok.mp3").toExternalForm()));
-    BackGround2MediaPlayer = new MediaPlayer(new Media(getClass().getResource("../Sound/happymusic.mp3").toExternalForm()));
+    BackGround1MediaPlayer = new MediaPlayer(new Media(getClass().getResource("../Music/tok.mp3").toExternalForm()));
+    BackGround2MediaPlayer = new MediaPlayer(new Media(getClass().getResource("../Music/happymusic.mp3").toExternalForm()));
     BackGround1MediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
     BackGround2MediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
   }
